3da99144a10d7f1e8e9ef4486fa668a29c6bae7e
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-07-22 04:19:00 -0300

    Add dynamic route dispatching.

--- server/MinimaServer.cs
+++ server/MinimaServer.cs
@@ -4,12 +4,16 @@ public class MinimaServer
 {
     private HttpListener listener;
     private int port;
+    private IDictionary<String, Action<HttpListenerRequest, HttpListenerResponse>> routes;
 
     public MinimaServer(int port)
     {
         this.port = port;
-        listener = new HttpListener();
-        listener.Prefixes.Add(String.Format("http://*:{0}/", port));
+
+        this.listener = new HttpListener();
+        this.listener.Prefixes.Add(String.Format("http://*:{0}/", port));
+
+        this.routes = new Dictionary<String, Action<HttpListenerRequest, HttpListenerResponse>>();
     }
 
     public void start()
@@ -21,15 +25,60 @@ public class MinimaServer
         {
             HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
             Console.WriteLine(String.Format("Got a new request: {0} {1} {2}", request.HttpMethod, request.Url, request.RawUrl));
 
-            HttpListenerResponse response = context.Response;
-            response.ContentType = "text/html";
-            response.StatusCode = 200;
+            bool missingRoute = true;
 
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes("<html><body>Hello world!</body></html>");
-            response.OutputStream.Write(buffer, 0, buffer.Length);
+            if (request.RawUrl != null)
+            {
+                try
+                {
+                    var handler = routes[request.RawUrl];
+                    missingRoute = false;
+                    try
+                    {
+                        handler(request, response);
+                    }
+                    catch (Exception e)
+                    {
+                        response.ContentType = "text/plain";
+                        response.StatusCode = 500;
+
+                        if (e.StackTrace != null)
+                        {
+                            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(e.StackTrace);
+                            response.OutputStream.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+                }
+                catch (KeyNotFoundException) { }
+            }
+
+            if (missingRoute)
+            {
+                Console.WriteLine(String.Format("Couldn't find route for path: {0}", request.RawUrl));
+
+                response.ContentType = "text/plain";
+                response.StatusCode = 404;
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes("not found");
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+
             response.OutputStream.Close();
+        }
+    }
+
+    public bool setRoute(string rawUrl, Action<HttpListenerRequest, HttpListenerResponse> handler)
+    {
+        try
+        {
+            routes.Add(rawUrl, handler);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
         }
     }
 
--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,18 @@
 ﻿MinimaServer server = new MinimaServer(8080);
+
 Console.CancelKeyPress += new ConsoleCancelEventHandler(server.Console_CancelKeyPress);
+
+server.setRoute("/", (request, response) =>
+{
+    response.StatusCode = 200;
+    response.ContentType = "application/json";
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes("{\"status\": \"ok\"}");
+    response.OutputStream.Write(buffer, 0, buffer.Length);
+});
+
+server.setRoute("/error", (request, response) =>
+{
+    throw new Exception("Error");
+});
+
 server.start();
